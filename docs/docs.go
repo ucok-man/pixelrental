4d7fe31b158c869b1297f1a90e6902a341f79c33
ucok-man <ucokkocu411@gmail.com>
2024-01-12 05:50:11 +0700

    fix: swagger api doc

--- docs/docs.go
+++ docs/docs.go
@@ -2232,7 +2232,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "pixelrental-production.up.railway.app:8080",
 	BasePath:         "/v1",
 	Schemes:          []string{},
 	Title:            "PixelRental API",
